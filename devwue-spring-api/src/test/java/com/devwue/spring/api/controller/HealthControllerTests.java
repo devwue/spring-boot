05bd0e973e2c926c084a31bbe4edf49376cc0319
김대종/익스트라넷실 <daijong.kim@yanolja.com>
2021-02-19 19:51:13 +0900

    Controller Test

---
+++ devwue-spring-api/src/test/java/com/devwue/spring/api/controller/HealthControllerTests.java
@@ -0,0 +1,30 @@
+package com.devwue.spring.api.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ActiveProfiles("local")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class HealthControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/health").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("ok")));
+    }
+}
